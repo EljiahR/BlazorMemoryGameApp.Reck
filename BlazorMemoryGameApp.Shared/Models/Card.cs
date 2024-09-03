aed35ba39e51f384a2915e2e8a84d84181ef43f2
Elijah <kcerhajile@gmail.com>
2024-09-03 07:57:55 -0400

    Created a shared project for models, added a temporary card model for testing

---
+++ BlazorMemoryGameApp.Shared/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMemoryGameApp.Shared.Models;
+
+public class Card
+{
+	public int Id { get; set; }
+	public string Content { get; set; }
+}
