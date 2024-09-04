df6b954303652fb89e2e9e8e626a1922d920d135
Elijah <kcerhajile@gmail.com>
2024-09-04 08:25:21 -0400

    Created a Games model for tracking results of finished games

---
+++ BlazorMemoryGameApp.Shared/Models/Games.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMemoryGameApp.Shared.Models;
+
+public class Games
+{
+	public int Id { get; set; }
+	[Required]
+	public DateTime DatePlayed { get; set; }
+	[Required]
+	public TimeSpan Duration { get; set; }
+	public string? GameType { get; set; }
+	public string? Difficulty { get; set; }
+	public int? FinishedRounds { get; set; }
+}
